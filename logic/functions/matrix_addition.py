b71625d3daed4a01e3244c493344751d0eed76de
Norvent <gordlab@mail.ru>
2025-01-21 13:05:27 +1000

    deletes unnecessary files

--- logic/functions/matrix_addition.py
+++
@@ -1,8 +0,0 @@
-from classes.Matrix_class import Matrix
-
-
-def matrix_addition() -> object:
-    first_parameter = Matrix.matrix_creation()
-    second_parameter = Matrix.matrix_creation()
-
-    return Matrix.plus(first_object=first_parameter, second_object=second_parameter)
--- logic/functions/matrix_find_determinator.py
+++
@@ -1,6 +0,0 @@
-from classes.Matrix_class import Matrix
-
-
-def find_determinator() -> float:
-    first_parameter = Matrix.matrix_creation()
-    return Matrix.find_determinator(matrix=first_parameter)
